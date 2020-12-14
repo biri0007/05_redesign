ba3f73e87ded1d0e07b448364d4bf38fb2bed1ed
Birita Yoon Vilhelm <biritayoonvilhelm@Biritas-MacBook-Pro.local>
2020-12-14 18:59:50 +0100

    slide galleri... not

--- om.js
+++ om.js
@@ -1,40 +1,33 @@
 //media query der lytter til str på browseren
 
 function myFunction(x) {
+
     if (x.matches) { // If media query matches
-        document.body.style.backgroundColor = "yellow";
-    } else {
-        document.body.style.backgroundColor = "pink";
+
+        var slideIndex = 1;
+        showDivs(slideIndex);
+
+        function plusDivs(n) {
+            showDivs(slideIndex += n);
+        }
+
+        function showDivs(n) {
+            var i;
+            var x = document.getElementsByClassName("galleri_img");
+            if (n > x.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = x.length
+            }
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            x[slideIndex - 1].style.display = "block";
+        }
     }
 }
 
 var x = window.matchMedia("(max-width: 550px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
-
-
-
-//function der laver slide på galleri
-
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("galleri_img");
-    if (n > x.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = x.length
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
-}
